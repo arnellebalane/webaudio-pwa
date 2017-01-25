17dbd33c77911b4ba4fc0269b886f4932c0eac35
Arnelle Balane <arnellebalane@gmail.com>
2017-01-25 22:30:34 +0800

    stream audio data from device mic to visualize

--- main.js
+++ main.js
@@ -1,44 +1,46 @@
-const audio = new Audio();
-audio.src = 'audio.mp3';
-audio.autoplay = true;
-audio.loop = true;
+const options = { audio: true };
+navigator.webkitGetUserMedia(options, capture, console.error);
 
-const audioCtx = new AudioContext();
-const source = audioCtx.createMediaElementSource(audio);
-const analyser = audioCtx.createAnalyser();
-source.connect(analyser);
-analyser.connect(audioCtx.destination);
 
-const audioData = new Float32Array(analyser.fftSize);
+function capture(stream) {
+  const audioCtx = new AudioContext();
+  const source = audioCtx.createMediaStreamSource(stream);
+  const analyser = audioCtx.createAnalyser();
+  source.connect(analyser);
+  analyser.connect(audioCtx.destination);
 
-const canvas = document.querySelector('canvas');
-const canvasCtx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+  const audioData = new Float32Array(analyser.fftSize);
 
-(function visualize() {
-    requestAnimationFrame(visualize);
-    analyser.getFloatTimeDomainData(audioData);
+  const canvas = document.querySelector('canvas');
+  const canvasCtx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-    const horizontalInterval = canvas.width / audioData.length;
+  (function visualize() {
+      requestAnimationFrame(visualize);
+      analyser.getFloatTimeDomainData(audioData);
 
-    canvasCtx.fillStyle = '#3F51B5';
-    canvasCtx.strokeStyle = '#FFC107';
-    canvasCtx.lineWidth = 3;
-    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-    canvasCtx.beginPath();
+      const horizontalInterval = canvas.width / audioData.length;
 
-    for (let i = 0; i < audioData.length; i++) {
-        const data = audioData[i];
-        const x = i * horizontalInterval;
-        const y = (canvas.height / 2) + (data * 250);
+      canvasCtx.fillStyle = '#222222';
+      canvasCtx.lineWidth = 1;
+      canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
 
-        if (i === 0) {
-            canvasCtx.moveTo(x, y);
-        } else {
-            canvasCtx.lineTo(x, y);
-        }
-    }
+      for (let i = 0; i < audioData.length; i++) {
+          const data = audioData[i] * 250;
+          const x = i * horizontalInterval;
+          const y = canvas.height / 2;
 
-    canvasCtx.stroke();
-})();
+          if (data >= 0) {
+            canvasCtx.strokeStyle = '#FFC107';
+          } else {
+            canvasCtx.strokeStyle = '#673AB7';
+          }
+
+          canvasCtx.beginPath();
+          canvasCtx.moveTo(x, y - data);
+          canvasCtx.lineTo(x, y + data);
+          canvasCtx.stroke();
+      }
+  })();
+}
