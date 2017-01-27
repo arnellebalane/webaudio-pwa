bffff503984055bcd0d1640c023fffbd7d1e661b
Arnelle Balane <arnellebalane@gmail.com>
2017-01-27 15:42:11 +0800

    simplify service worker code

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'cache-v1';
+const cacheName = 'cache-v2';
 const pathsToCache = [
   '/',
   '/main.css',
@@ -8,33 +8,21 @@ const pathsToCache = [
 ];
 
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll(pathsToCache))
-      .then(_ => console.info(`Caching for "${cacheName}" complete.`))
-      .then(_ => self.skipWaiting())
-  );
-});
+self.addEventListener('install', e => e.waitUntil(
+  caches.open(cacheName)
+    .then(cache => cache.addAll(pathsToCache))
+    .then(_ => self.skipWaiting())
+));
 
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(key => {
-        if (key !== cacheName) {
-          return caches.delete(key);
-        }
-      })))
-      .then(_ => console.info(`Deleted old caches.`))
-      .then(_ => self.clients.claim())
-  );
-});
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys()
+    .then(keys => Promise.all(keys.map(key => (key !== cacheName) && caches.delete(key))))
+    .then(_ => self.clients.claim())
+));
 
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(response => response || fetch(e.request))
-  );
-});
+self.addEventListener('fetch', e => e.respondWith(
+  caches.match(e.request)
+    .then(response => response || fetch(e.request))
+));
