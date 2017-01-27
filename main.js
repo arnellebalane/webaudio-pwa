c701aeeb92f0f45fbb6bfe21ba89b7eaca837035
Arnelle Balane <arnellebalane@gmail.com>
2017-01-27 15:32:42 +0800

    refactor main script to be more flexible

--- main.js
+++ main.js
@@ -1,46 +1,58 @@
-const options = { audio: true };
-navigator.webkitGetUserMedia(options, capture, console.error);
+const audioCtx = new AudioContext();
+const analyser = audioCtx.createAnalyser();
+
+const audioData = new Float32Array(analyser.fftSize);
+let source = null;
 
 
-function capture(stream) {
-  const audioCtx = new AudioContext();
-  const source = audioCtx.createMediaStreamSource(stream);
-  const analyser = audioCtx.createAnalyser();
+const canvas = document.querySelector('canvas');
+const canvasCtx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height =  window.innerHeight;
+
+let backgroundColor = '#111111';
+let visualizationColor = '#ffc107';
+
+
+const constraints = { audio: true };
+navigator.mediaDevices.getUserMedia(constraints)
+  .then(visualize)
+  .catch(console.error);
+
+
+function visualize(input) {
+  if (source) {
+    source.disconnect();
+  }
+  if (input instanceof HTMLMediaElement) {
+    source = audioCtx.createMediaElementSource(input);
+    analyser.connect(audioCtx.destination);
+  } else if (input instanceof MediaStream) {
+    source = audioCtx.createMediaStreamSource(input);
+    analyser.disconnect();
+  } else {
+    throw new Error('Unknown audio input type.');
+  }
   source.connect(analyser);
-  analyser.connect(audioCtx.destination);
-
-  const audioData = new Float32Array(analyser.fftSize);
-
-  const canvas = document.querySelector('canvas');
-  const canvasCtx = canvas.getContext('2d');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  (function visualize() {
-      requestAnimationFrame(visualize);
-      analyser.getFloatTimeDomainData(audioData);
-
-      const horizontalInterval = canvas.width / audioData.length;
-
-      canvasCtx.fillStyle = '#222222';
-      canvasCtx.lineWidth = 1;
-      canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-      for (let i = 0; i < audioData.length; i++) {
-          const data = audioData[i] * 250;
-          const x = i * horizontalInterval;
-          const y = canvas.height / 2;
-
-          if (data >= 0) {
-            canvasCtx.strokeStyle = '#FFC107';
-          } else {
-            canvasCtx.strokeStyle = '#673AB7';
-          }
-
-          canvasCtx.beginPath();
-          canvasCtx.moveTo(x, y - data);
-          canvasCtx.lineTo(x, y + data);
-          canvasCtx.stroke();
-      }
-  })();
 }
+
+
+(function render() {
+  requestAnimationFrame(render);
+  analyser.getFloatTimeDomainData(audioData);
+
+  canvasCtx.fillStyle = backgroundColor;
+  canvasCtx.strokeStyle = visualizationColor;
+  canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0; i < audioData.length; i++) {
+    const data = audioData[i] * 250;
+    const x = canvas.width / audioData.length * i;
+    const y = canvas.height / 2;
+
+    canvasCtx.beginPath();
+    canvasCtx.moveTo(x, y - data);
+    canvasCtx.lineTo(x, y + data);
+    canvasCtx.stroke();
+  }
+})();
